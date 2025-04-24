e3e78a2c5133a578e8e147541e57992bfcdb1f66
Eugene <mr.difiko@yandex.ru>
2025-04-24 21:24:20 +0700

    fix test script

--- Assets/Scripts/Audio/PlayRandomClip.cs
+++ Assets/Scripts/Audio/PlayRandomClip.cs
@@ -5,7 +5,7 @@ namespace WinterUniverse
 {
     public class PlayRandomClip : MonoBehaviour
     {
-        [SerializeField] private string _testClipEventPath;
+        [SerializeField] private EventReference _testClipEventPath;
         [SerializeField] private bool _testPlayOneShot;
         [SerializeField] private bool _testPlayIn3D;
         [SerializeField] private Vector3 _testPlayPosition;
@@ -13,15 +13,10 @@ namespace WinterUniverse
         [SerializeField] private bool _testPlayAttached;
         [SerializeField] private Transform _testAttachTarget;
 
-        private void Update()
+        private void OnEnable()
         {
-            if(_testClipEventPath == "")
-            {
-                return;
-            }
             if (_testPlayOneShot)
             {
-                _testPlayOneShot = false;
                 if (_testPlayIn3D)
                 {
                     if (_testPlayTransform != null)
@@ -38,9 +33,8 @@ namespace WinterUniverse
                     RuntimeManager.PlayOneShot(_testClipEventPath);
                 }
             }
-            if (_testPlayAttached && _testAttachTarget != null)
+            else if (_testPlayAttached && _testAttachTarget != null)
             {
-                _testPlayAttached = false;
                 RuntimeManager.PlayOneShotAttached(_testClipEventPath, _testAttachTarget.gameObject);
             }
         }
